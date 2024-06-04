d9a8a6079e476fafe1f802708ebe6d9465d469da
YanLin <yan0988777698@gmail.com>
2024-06-04 13:57:58 +0800

    #Login Controller Finished

---
+++ projRESTfulApiFitConnect/Controllers/LogInController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projRESTfulApiFitConnect.DTO.LogIn;
+using projRESTfulApiFitConnect.Models;
+
+namespace projRESTfulApiFitConnect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogInController : ControllerBase
+    {
+        GymContext _context;
+        public LogInController(GymContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm]PostIdentityDto postIdentityDto)
+        {
+            var identity =await _context.TIdentities.FirstOrDefaultAsync(x => (x.Phone == postIdentityDto.username || x.EMail == postIdentityDto.username) && x.Password == postIdentityDto.password);
+            var result = new
+            {
+                identity.Id,
+                identity.RoleId
+            };
+            return Ok(result);
+        }
+    }
+}
---
+++ projRESTfulApiFitConnect/DTO/LogIn/PostIdentityDto.cs
@@ -0,0 +1,8 @@
+﻿namespace projRESTfulApiFitConnect.DTO.LogIn
+{
+    public class PostIdentityDto
+    {
+        public string? username { get; set; }
+        public string? password { get; set; }
+    }
+}
