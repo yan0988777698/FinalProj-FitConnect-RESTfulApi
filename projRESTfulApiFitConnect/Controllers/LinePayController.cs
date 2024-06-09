d6272850841ea4ca544bb80954c4aa6c475cd749
彥霖 陳 <yan0988777698@gmail.com>
2024-06-09 12:54:13 +0800

    #LinePay

---
+++ projRESTfulApiFitConnect/Controllers/LinePayController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using projRESTfulApiFitConnect.LinePay.Domain;
+using projRESTfulApiFitConnect.LinePay.Dtos;
+
+namespace projRESTfulApiFitConnect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinePayController : ControllerBase
+    {
+        private readonly LinePayService _linePayService;
+        public LinePayController()
+        {
+            _linePayService = new LinePayService();
+        }
+
+        [HttpPost("Create")]
+        public async Task<PaymentResponseDto> CreatePayment(PaymentRequestDto dto)
+        {
+            return await _linePayService.SendPaymentRequest(dto);
+        }
+
+        [HttpPost("Confirm")]
+        public async Task<PaymentConfirmResponseDto> ConfirmPayment([FromQuery] string transactionId, [FromQuery] string orderId, PaymentConfirmDto dto)
+        {
+            return await _linePayService.ConfirmPayment(transactionId, orderId, dto);
+        }
+
+        [HttpGet("Cancel")]
+        public async void CancelTransaction([FromQuery] string transactionId)
+        {
+            _linePayService.TransactionCancel(transactionId);
+        }
+    }
+}
---
+++ projRESTfulApiFitConnect/Controllers/SchduleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace projRESTfulApiFitConnect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchduleController : ControllerBase
+    {
+        
+    }
+}
